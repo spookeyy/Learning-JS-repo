41d9f78fb41b7fb6eb17d9efb9937569a38a7761
spookeyy <pangasmeshack@gmail.com>
2024-04-02 06:22:20 +0300

    Initial commit

--- class/array2.js
+++ class/array2.js
@@ -2,6 +2,7 @@ students = ["kim", "tom", "kyle", "lexy", "Jerry", "Hector"]
 // students.slice(1, 3)
 // console.log(students.slice(3));
 
+console.log("students[0].toUpperCase()" + students[0].substring(1));
 
 const cats = ["Milo", "Otis", "Garfield"];
 function appendCat(name) {
