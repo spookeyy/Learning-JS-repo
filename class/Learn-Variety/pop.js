efe4b6e05b649d447c2df6f10759157bda3c6a8b
spookeyy <pangasmeshack@gmail.com>
2024-05-11 23:44:28 +0300

    pop and shift

---
+++ class/Learn-Variety/pop.js
@@ -0,0 +1,11 @@
+//removing element from an array
+
+//pop
+const cities = ["London", "Paris", "New York", "Sydney"]
+cities.pop()
+console.log(cities) //removes the last item in the array
+
+//shift
+const days = ["Monday", "Tuesday", "Wednesday", "Thursday"]
+days.shift()
+console.log(days) //removes the first item in the array
