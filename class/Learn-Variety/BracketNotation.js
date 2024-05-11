da8a23cda77a8bc328fa6d5e275f4d8098e61caf
spookeyy <pangasmeshack@gmail.com>
2024-05-12 00:18:50 +0300

    bracket Notation

---
+++ class/Learn-Variety/BracketNotation.js
@@ -0,0 +1,10 @@
+//using bracket notation to replace elements in an array
+
+const days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
+days[1] = "TUESDAY"; //replaces index 1
+console.log(days)
+
+const cards = ["queen of hearts", "jack of clubs", "ten of diamonds", "ace of spades"];
+cards[2] = "king of diamonds"
+console.log(cards)
+
