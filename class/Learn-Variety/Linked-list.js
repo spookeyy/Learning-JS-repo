1a96e2f47df04ffa7ddc53439948031fe5147ce3
spookeyy <pangasmeshack@gmail.com>
2024-06-10 00:08:08 +0300

    singly linked list in js

---
+++ class/Learn-Variety/Linked-list.js
@@ -0,0 +1,13 @@
+// Time Complexity: O(N)
+// Auxiliary Space: O(N)
+
+//Linked List
+// node of a singly linked list
+class Node {
+    constructor(){ // constructor of a node
+        this.data = 0 // data of a node 
+        this.prev = null // previous node
+        this.next = null // next node
+    }
+}
+
