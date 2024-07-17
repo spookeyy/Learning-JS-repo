499ec716105de031e4f946c5d5cfc089a023f77f
spookeyy <pangasmeshack@gmail.com>
2024-07-17 14:53:22 +0300

    add 2 numbers leet code

--- leetcode/add-two-numbers.js
+++ leetcode/add-two-numbers.js
@@ -1,43 +1,52 @@
-// linked list
-// https://leetcode.com/problems/add-two-numbers/
-// class ListNode {
-//   constructor(value) {
-//     this.value = value;
-//     this.next = null;
-//   }
-// }
-
-function ListNode(value) {
-  this.value = value;
+function ListNode(val) {
+  this.val = val;
   this.next = null;
 }
 
-
 var addTwoNumbers = function (l1, l2) {
-//solution
-    let dummy = new ListNode();
-    let curr = dummy;
-    let carry = 0;
-    while (l1 || l2 || carry) {
-        let sum = 0;
-        if (l1) {
-            sum += l1.val;
-            l1 = l1.next;
-        }
-        if (l2) {
-            sum += l2.val;
-            l2 = l2.next;
-        }
-        sum += carry;
-        carry = Math.floor(sum / 10);
-        curr.next = new ListNode(sum % 10);
-        curr = curr.next;
+  let dummy = new ListNode(0);
+  let curr = dummy;
+  let carry = 0;
+  while (l1 || l2 || carry) {
+    let sum = carry;
+    if (l1) {
+      sum += l1.val;
+      l1 = l1.next;
     }
-    return dummy.next
-
+    if (l2) {
+      sum += l2.val;
+      l2 = l2.next;
+    }
+    carry = Math.floor(sum / 10);
+    curr.next = new ListNode(sum % 10);
+    curr = curr.next;
+  }
+  return dummy.next;
 };
 
-// Input: l1 = [2,4,3], l2 = [5,6,4]
-// Output: [7,0,8]
-// Explanation: 342 + 465 = 807.
-console.log(addTwoNumbers([2, 4, 3], [5, 6, 4]))
+// Helper function to create a linked list from an array
+function createLinkedList(arr) {
+  let dummy = new ListNode(0);
+  let curr = dummy;
+  for (let val of arr) {
+    curr.next = new ListNode(val);
+    curr = curr.next;
+  }
+  return dummy.next;
+}
+
+// Helper function to convert linked list to array
+function linkedListToArray(head) {
+  let arr = [];
+  while (head) {
+    arr.push(head.val);
+    head = head.next;
+  }
+  return arr;
+}
+
+// Test
+let l1 = createLinkedList([2, 4, 3]);
+let l2 = createLinkedList([5, 6, 4]);
+let result = addTwoNumbers(l1, l2);
+console.log(linkedListToArray(result)); // Output: [7, 0, 8]
